8444aa79354bd44e20bfbfd55b8b592ef6544e07
jchaut <j.chaut@3kles-consulting.com>
2021-07-25 23:11:40 +0200

    Add function to buildSecureRouter

--- src/mongodb.utils.ts
+++ src/mongodb.utils.ts
@@ -1,6 +1,8 @@
+import { IAuth } from '3kles-corebe';
 import * as mongoose from 'mongoose';
 import { MongoDBController } from "./mongodb.controller";
 import { MongoDBRouter } from "./mongodb.router";
+import { MongoDBSecureRouter } from './mongodb.secure.router';
 import { MongoDBService } from "./mongodb.service";
 
 export class MongoDBUtils {
@@ -9,5 +11,11 @@ export class MongoDBUtils {
 		const service: MongoDBService = new MongoDBService(model);
 		const controller: MongoDBController = new MongoDBController(service);
 		return new MongoDBRouter(controller);
+	}
+
+	public static buildSecureRouterFromModel(model: mongoose.Model<mongoose.Document>, auth: IAuth): MongoDBRouter {
+		const service: MongoDBService = new MongoDBService(model);
+		const controller: MongoDBController = new MongoDBController(service);
+		return new MongoDBSecureRouter(auth, controller);
 	}
 }
