cf5057b772f273a3b933af24862ea26c5e40b279
Charles Da Silva Costa <CDASILVA@3KLES.local>
2025-07-31 09:37:25 +0200

    fix: corebe version & mongodb controller router order

--- src/mongodb.router.ts
+++ src/mongodb.router.ts
@@ -4,23 +4,20 @@ import { Router, RouterOptions } from 'express';
 
 // Class to create a mongodb router from a GenericController
 export class MongoDBRouter<T = any> extends GenericRouter {
+    constructor(controller: MongoDBController<T>, options?: RouterOptions) {
+        super(controller, options);
+    }
 
-	constructor(controller: MongoDBController<T>, options?: RouterOptions) {
-		super(controller, options);
-	}
+    public addController(controller: MongoDBController<T>, checker?: any): void {
+        super.addController(controller, checker);
 
-	public addController(controller: MongoDBController<T>, checker?: any): void {
-
-		const crudRouter = Router(this.options);
-		crudRouter.route('/').get(controller.execute('list'));
-		crudRouter.route('/:id').get(controller.execute('get'));
-		crudRouter.route('/:id').put(controller.execute('update'));
-		crudRouter.route('/:id').patch(controller.execute('update'));
-		crudRouter.route('/').post(controller.execute('add'));
-		crudRouter.route('/:id').delete(controller.execute('delete'));
-
-		this.router.use(`/${controller.getParameters().modelname}`, crudRouter);
-		super.addController(controller, checker);
-	}
-
+        const crudRouter = Router(this.options);
+        crudRouter.route('/').get(controller.execute('list'));
+        crudRouter.route('/:id').get(controller.execute('get'));
+        crudRouter.route('/:id').put(controller.execute('update'));
+        crudRouter.route('/:id').patch(controller.execute('update'));
+        crudRouter.route('/').post(controller.execute('add'));
+        crudRouter.route('/:id').delete(controller.execute('delete'));
+        this.router.use(`/${controller.getParameters().modelname}`, crudRouter);
+    }
 }
